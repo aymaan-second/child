18f98ab02e71b33d4069d6ee9ddacd409e30c362
aymaan <aymaanpathan5@gmail.com>
2024-12-26 15:36:41 +0530

    add sort without build

--- index.js
+++ index.js
@@ -1,3 +1,41 @@
-function sort(nums) {
-  return nums.sort();
+function mergeSort(nums) {
+  if (nums.length == 1) {
+    return nums;
+  }
+  let mid = Math.floor(nums.length / 2);
+  let left = nums.slice(0, mid);
+  let right = nums.slice(mid);
+
+  let leftSorted = mergeSort(left);
+  let rightSorted = mergeSort(right);
+  return merge(leftSorted, rightSorted);
 }
+function merge(left, right) {
+  let i = 0;
+  let j = 0;
+  let nums = [];
+  let k = 0;
+  while (i < left.length && j < right.length) {
+    if (left[i] < right[j]) {
+      nums.push(left[i]);
+      i++;
+    } else {
+      nums.push(right[j]);
+      j++;
+    }
+    k++;
+  }
+  while (i < left.length) {
+    nums.push(left[i]);
+    i++;
+    k++;
+  }
+  while (j < right.length) {
+    nums.push(right[j]);
+    j++;
+    k++;
+  }
+  return nums;
+}
+const arr = [90, 45, 67, 86, 12, 43];
+console.log(mergeSort(arr));
